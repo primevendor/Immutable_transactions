c1b9735bb6923ee7321148e7559e9347ec7f54e7
Divya-Vasa <divyajyothivasa@gmail.com>
2025-02-01 00:16:27 -0600

    Add files via upload

---
+++ Project_folder/frontend/src/components/Footer.jsx
@@ -0,0 +1,30 @@
+
+export default function Footer() {
+    
+    return (
+        <div style={{padding: "12px 0px", backgroundColor : "rgb(232, 227, 227)", textAlign: 'center', position:'fixed', width: '100%',height : '85px', bottom : '0', left: '0', right: '0', zIndex : '999'}}>
+            
+            <div className="text-center">
+                    
+            </div> 
+
+            <div className="text-center pt-1">
+                <a href="https://www.linkedin.com/in/divya-vasa-57bb442b7/" target="_blank" rel="noreferrer">
+                    <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
+                </a>
+                
+                
+
+                <a href="https://github.com/Divya-Vasa" target="_blank" rel="noreferrer">
+                    <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
+                <a href="mailto:divyajyothivasa@gmail.com" target="_blank" rel="noreferrer">
+                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+            </div>
+            {/* <footer style={{backgroundColor: 'crimson', color:'springgreen', textAlign: 'center', position:'fixed', width: '100%',height : '50px', bottom : '0', left: '0', right: '0', zIndex : '999'}}><h1>THIS IS MY FOOTER</h1></footer> */}
+        </div>
+       
+    )
+}
